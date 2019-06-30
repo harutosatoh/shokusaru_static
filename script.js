91b060d4852a744e89864fa9b70325b669046b40
harutosatoh <satohchan1197@gmail.com>
2019-06-30 09:35:55 +0900

    footer-added

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 $(function(){
 
+    $(".map").click(function(){
+        $("#footer-modal-wrapper").fadeIn(1500);
+    });
 
+        //スクロール時に呼ばれる
+        $(window).scroll(function(){
+            const top = $(window).scrollTop();    //縦スクロール量を取得
+            const left = $(window).scrollLeft();  //横スクロール量を取得
 
+          if(top>=250){
+              $(".main-title").fadeIn(1200);
+           }
 
+          if(top>=1000){
+              $(".main-information").fadeIn(1000);
+          }
+        });
 
-$('img').slideDown();
+      $(".close-modal").click(function(){
+          $("#footer-modal-wrapper").fadeOut();
+      })
 
 });
+
+
